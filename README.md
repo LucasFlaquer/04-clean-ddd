286db82aefead8ca17558754ad1fdfb42984d906
LucasFlaquer <lucas.flaquer@gmail.com>
2023-11-27 23:35:21 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Ignite Fórum
+
+## Entities
+
+## Use-cases
+
+
+
+
+# DDD (Domain-driven design)
+ ## Domínio
+ - Domain experts
+ - Conversa
+ - Linguagem ubíqua
+
+-usuário
+  - Cliente
+  - Fornecedor
+  - 
+
+
+
+
+
+representar um problema da vida real em código sem qualquer camada externa
+
---
+++ src/domain/use-cases/answer-question.ts
@@ -0,0 +1,14 @@
+import { Answer } from '../entities/answer'
+
+interface Input {
+  instructorId: string
+  questionId: string
+  content: string
+}
+
+export class AnswerQuestionUseCase {
+  execute({instructorId, questionId, content}:Input) {
+    const answer = new Answer(content)
+    return answer
+  }
+}
